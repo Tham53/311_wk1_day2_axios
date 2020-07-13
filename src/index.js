0134e71f2f7ba86eda981e150a8de8faed15e385
THam53 <texas01988@gmail.com>
2020-07-12 21:20:44 -0500

    new

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 // import axios here
-
+const axios = require('axios');
+const config = require('../config');
+// console.log(config.api_key)
 // Please note that it is normally not considered best practice to commit 
 // api keys to github as it presents a security risk. It is done here only 
 // for practice purposes as we are sharing the same account
@@ -8,20 +10,23 @@ const api_key = 'd771b19ef336ed8381def3a60b574464'
 const discoverMovie = () => {
   const url = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}`
   // code here
+  return axios(url).then(response => response)
 }
 
 const getMovieById = (id) => {
   const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${api_key}`
   // code here
+  return axios(url, {params: {id: 500}}).then(response=>response)
+  return response.data.original_title
 }
 
 const getMovieByIdFailure = () => {
   const fakeId = 1 // FAKE ID HERE
   const url = `https://api.themoviedb.org/3/movie/${fakeId}?api_key=${api_key}`
   // code here
+  return axios(url).then(response=>response){
 }
-
-
+}
 
 module.exports = {
   discoverMovie,
